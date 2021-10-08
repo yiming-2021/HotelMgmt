993da637947d8d78be4c426d7be32e88febb6083
Yiming <90655469+yiming-2021@users.noreply.github.com>
2021-10-08 13:52:21 -0400

    Update ManageServices.cs

--- HotelMgmt.ConsoleApp/ManageServices.cs
+++ HotelMgmt.ConsoleApp/ManageServices.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using HotelMgmt.Data.Model;
@@ -23,9 +23,69 @@ namespace HotelMgmt.ConsoleApp
             }
         }
 
+        void AddServices()
+        {
+            Services sv = new Services();
+
+            Console.Write("Enter Room Num = ");
+            sv.ROOMNO = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Service Desciption = ");
+            sv.SDESC = Console.ReadLine();
+            Console.Write("Enter Amount = ");
+            sv.AMOUNT = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Service Date = ");
+            sv.ServiceDate = Convert.ToDateTime(Console.ReadLine());
+
+            servicesRepository.Insert(sv);
+            Console.WriteLine("Service has been added successfully");
+
+        }
+
+
+        void DeleteServices()
+        {
+            Rooms rt = new Rooms();
+            Console.Write("Enter Id to delete= ");
+            int Id = Convert.ToInt32(Console.ReadLine());
+
+            int n = servicesRepository.Delete(Id);
+
+            if (n > 0)
+                Console.WriteLine("Service deleted.");
+
+        }
+
+
+
         public override void Run()
         {
-            PrintAll();
+            int choice = 0;
+            do
+            {
+                Console.Clear();
+                Menu m = new Menu();
+                choice = m.Print(typeof(Operations));
+                switch (choice)
+                {
+                    case (int)Operations.Add:
+                        AddServices();
+                        break;
+                    case (int)Operations.Delete:
+                        DeleteServices();
+                        break;
+                    case (int)Operations.Print:
+                        PrintAll();
+                        break;
+                    case (int)Operations.Exit:
+                        Console.WriteLine("Thanks for visit!!!!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+                Console.WriteLine("Press Enter to continue.....");
+                Console.ReadLine();
+            } while (choice != (int)Operations.Exit);
         }
     }
 }
