ae792c663a674ef83e7e896bb74da339f53e2820
Yiming <90655469+yiming-2021@users.noreply.github.com>
2021-10-08 14:08:11 -0400

    Update ManageCustomers.cs

--- HotelMgmt.ConsoleApp/ManageCustomers.cs
+++ HotelMgmt.ConsoleApp/ManageCustomers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using HotelMgmt.Data.Model;
@@ -23,9 +23,80 @@ namespace HotelMgmt.ConsoleApp
             }
         }
 
+        void AddCustomers()
+        {
+            Customers c = new Customers();
+
+            Console.Write("Enter Room Num = ");
+            c.ROOMNO = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Customer Name = ");
+            c.CNAME = Console.ReadLine();
+            Console.Write("Enter Address = ");
+            c.ADDRESS = Console.ReadLine();
+            Console.Write("Enter Phone num = ");
+            c.PHONE = Console.ReadLine();
+            Console.Write("Enter Email = ");
+            c.EMAIL = Console.ReadLine();
+            Console.Write("Enter Check in date = ");
+            c.CHECKIN = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Enter Total Persons = ");
+            c.TotalPERSONS = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Booking days = ");
+            c.BookingDays = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Booking days = ");
+            c.ADVANCE = Convert.ToDecimal(Console.ReadLine());
+
+
+            customersRepository.Insert(c);
+            Console.WriteLine("Customer has been added successfully");
+
+        }
+
+
+        void DeleteCustomers()
+        {
+            Customers c = new Customers();
+            Console.Write("Enter Id to delete= ");
+            int Id = Convert.ToInt32(Console.ReadLine());
+
+            int n = customersRepository.Delete(Id);
+
+            if (n > 0)
+                Console.WriteLine("Customer deleted.");
+
+        }
+
+
+
         public override void Run()
         {
-            PrintAll();
+            int choice = 0;
+            do
+            {
+                Console.Clear();
+                Menu m = new Menu();
+                choice = m.Print(typeof(Operations));
+                switch (choice)
+                {
+                    case (int)Operations.Add:
+                        AddCustomers();
+                        break;
+                    case (int)Operations.Delete:
+                        DeleteCustomers();
+                        break;
+                    case (int)Operations.Print:
+                        PrintAll();
+                        break;
+                    case (int)Operations.Exit:
+                        Console.WriteLine("Thanks for visit!!!!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+                Console.WriteLine("Press Enter to continue.....");
+                Console.ReadLine();
+            } while (choice != (int)Operations.Exit);
         }
     }
 }
